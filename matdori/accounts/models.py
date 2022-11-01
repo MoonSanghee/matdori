c34e9af45a512ac1722178096282d6de9f30a89e
myeonghwna57 <myeonghwan57@gmail.com>
2022-11-01 11:14:55 +0900

    accounts update

--- matdori/accounts/models.py
+++ matdori/accounts/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
+
 class User(AbstractUser):
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers"
+    )
+    nickname = models.CharField(max_length=80)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(100, 100)],
+        format="JPEG",
+        options={"quality": 80},
+    )
