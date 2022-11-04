3546cd30a1c6b80aaf0c8722bd284be1522e5e2c
suyoung049 <suyoung049@gmail.com>
2022-11-04 16:05:05 +0900

    search/detail

--- matdori/posts/views.py
+++ matdori/posts/views.py
@@ -8,60 +8,58 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 
-
 # Create your views here.
 
+
 def index(request):
-    posts = Post.objects.order_by('-pk')
-    context = {
-        'posts':posts
-    }
-    return render(request, 'posts/index.html', context)
+    posts = Post.objects.order_by("-pk")
+    context = {"posts": posts}
+    return render(request, "posts/index.html", context)
+
 
 @login_required
 def create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         post_form = PostForm(request.POST, request.FILES)
         if post_form.is_valid():
             post = post_form.save(commit=False)
             post.user = request.user
             post.save()
-            return redirect('posts:index')
+            return redirect("posts:index")
     else:
         post_form = PostForm()
-    context = {
-        'post_form': post_form
-    }
-    return render(request, 'posts/form.html', context)
+    context = {"post_form": post_form}
+    return render(request, "posts/form.html", context)
+
 
 @login_required
 def update(request, pk):
     post = Post.objects.get(pk=pk)
     if request.user == post.user:
-        if request.method == 'POST':
+        if request.method == "POST":
             post_form = PostForm(request.POST, request.FILES, instance=post)
             if post_form.is_valid():
                 post_form.save()
-                return redirect('posts:detail', post.pk)
+                return redirect("posts:detail", post.pk)
         else:
             post_form = PostForm(instance=post)
-        context = {
-            'post_form':post_form
-        }
-        return render(request, 'posts/form.html', context)
+        context = {"post_form": post_form}
+        return render(request, "posts/form.html", context)
     else:
-        messages.warning(request, '작성자만 수정할 수 있습니다.')
-        return redirect('posts:detail', post.pk)
+        messages.warning(request, "작성자만 수정할 수 있습니다.")
+        return redirect("posts:detail", post.pk)
+
 
 @login_required
 def delete(request, pk):
     post = Post.objects.get(pk=pk)
     if request.user == post.user:
         post.delete()
-        return redirect('posts:index')
+        return redirect("posts:index")
     else:
-        messages.warning(request, '작성자만 삭제할 수 있습니다.')
-        return redirect('posts:detail', pk)
+        messages.warning(request, "작성자만 삭제할 수 있습니다.")
+        return redirect("posts:detail", pk)
+
 
 def detail(request, pk):
     post = Post.objects.get(pk=pk)
@@ -71,14 +69,15 @@ def detail(request, pk):
     for review in reviews:
         points += review.glade
     if len(reviews):
-        points = round(points/len(reviews), 1)
+        points = round(points / len(reviews), 1)
     context = {
-        'post':post,
-        'reviews':post.review_set.all(),
-        'reviewsform':reviewsform,
-        'points': points
+        "post": post,
+        "reviews": post.review_set.all(),
+        "reviewsform": reviewsform,
+        "points": points,
     }
-    return render(request, 'posts/detail.html', context)
+    return render(request, "posts/detail.html", context)
+
 
 @login_required
 def likes(request, pk):
@@ -89,61 +88,64 @@ def likes(request, pk):
     else:
         post.like_user.add(request.user)
         is_liked = True
-    context = {'isLiked': is_liked, 'likeCount': post.like_user.count()}
+    context = {"isLiked": is_liked, "likeCount": post.like_user.count()}
     return JsonResponse(context)
+
 
 @login_required
 def review_create(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         post_form = ReviewForm(request.POST, request.FILES)
         if post_form.is_valid():
             review = post_form.save(commit=False)
             review.user = request.user
             review.post = Post.objects.get(pk=pk)
             review.save()
-            return redirect('posts:detail', pk)
+            return redirect("posts:detail", pk)
     else:
         post_form = ReviewForm()
-    context = {
-        'post_form': post_form
-    }
-    return render(request, 'posts/form.html', context)
+    context = {"post_form": post_form}
+    return render(request, "posts/form.html", context)
+
 
 @login_required
 def review_delete(request, post_pk, review_pk):
     review = Review.objects.get(pk=review_pk)
     if review.user == request.user:
         review.delete()
-        return redirect('posts:detail', post_pk)
+        return redirect("posts:detail", post_pk)
     else:
-        messages.warning(request, '작성자만 삭제할 수 있습니다.')
-    return redirect('posts:detail', post_pk)
+        messages.warning(request, "작성자만 삭제할 수 있습니다.")
+    return redirect("posts:detail", post_pk)
+
 
 def search(requset):
-    searched = requset.GET.get('searched', False)
-    field = requset.GET.get('field')
-    if field == '1':
-        posts = Post.objects.filter(Q(title__contains=searched) | Q(address__contains=searched) | Q(sectors__contains=searched) | Q(characteristic__contains=searched)).order_by('-pk')
-    elif field == '2':
-        posts = Post.objects.filter(Q(title__contains=searched)).order_by('-pk')
-    elif field == '3':
-        posts = Post.objects.filter(Q(adress__contains=searched)).order_by('-pk')
-    elif field == '4':
-        posts = Post.objects.filter(Q(sectors__contains=searched)).order_by('-pk')
-    elif field == '5':
-        posts = Post.objects.filter(Q(characteristic__contains=searched)).order_by('-pk')
+    searched = requset.GET.get("searched", False)
+    field = requset.GET.get("field")
+    if field == "1":
+        posts = Post.objects.filter(
+            Q(title__contains=searched)
+            | Q(address__contains=searched)
+            | Q(sectors__contains=searched)
+            | Q(characteristic__contains=searched)
+        ).order_by("-pk")
+    elif field == "2":
+        posts = Post.objects.filter(Q(title__contains=searched)).order_by("-pk")
+    elif field == "3":
+        posts = Post.objects.filter(Q(adress__contains=searched)).order_by("-pk")
+    elif field == "4":
+        posts = Post.objects.filter(Q(sectors__contains=searched)).order_by("-pk")
+    elif field == "5":
+        posts = Post.objects.filter(Q(characteristic__contains=searched)).order_by(
+            "-pk"
+        )
     if not searched:
         posts = []
-        text = '검색어를 입력해 주세요.'
+        text = "검색어를 입력해 주세요."
     elif not len(posts):
-        text = '검색 결과가 없습니다.'
+        text = "검색 결과가 없습니다."
     else:
-        text = ''
-    context = {
-        'posts':posts,
-        'text':text,
-        'searched':searched,
-        'field':field
-    }
-    return render(requset, 'posts/search.html', context)
+        text = ""
+    context = {"posts": posts, "text": text, "searched": searched, "field": field}
+    return render(requset, "posts/search.html", context)
